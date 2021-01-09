830390cc715d69a8769ecaaa659b2775405c8816
Lisandro Corbalan <lmcorbalan@gmail.com>
2021-01-08 21:09:28 -0300

    Add setup-tokens command

---
+++ cb-sol-cli/cmd/setup-tokens-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  CBG_URL: "",
+  CBK_URL: "",
+  AVA_NETWORK_ID: 43113,
+  ETH_GAS_PRICE: 20000000000,
+  AVA_GAS_PRICE: 470000000000,
+  CB_PK: "",
+
+  CBG_ID: 1,
+  CBK_ID: 2,
+
+  CBG_BRIDGE: "",
+  CBG_ERC20HANDLER: "",
+
+  CBK_BRIDGE: "",
+  CBK_ERC20HANDLER: "",
+
+  ETH_TOKENS: [
+    {
+      address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+      name: "Dai Stablecoin",
+      symbol: "DAI"
+    },
+    {
+      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      name: "USD Coin",
+      symbol: "USDC"
+    }
+  ],
+
+  AVA_TOKENS: [
+    {
+      address: "0x1D308089a2D1Ced3f1Ce36B1FcaF815b07217be3",
+      name: "Wrapped AVAX",
+      symbol: "WAVAX"
+    }
+  ]
+}
